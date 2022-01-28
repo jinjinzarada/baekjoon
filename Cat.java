2bda50602a17d5bd15f82aac9b67adbeabc7bf56
jinjinzarada <jinjinzara774@gmail.com>
2022-01-28 22:27:05 +0900

    백준 입출력과 사칙연산 - 10171번

---
+++ Cat.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
