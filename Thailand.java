2b7ab268a67f16f86a2e7f94edc28f5e5a0e7020
jinjinzarada <jinjinzara774@gmail.com>
2022-02-04 21:10:59 +0900

    백준 입출력과 사칙연산 - 18108번

---
+++ Thailand.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.util.Scanner;
+
+public class Thailand {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i= sc.nextInt();
+		System.out.println(i-543);
+		
+	}
+
+}
