7b05b60d17b76834de78aad2991bd504e0af48e9
jinjinzarada <jinjinzara774@gmail.com>
2022-02-10 22:37:01 +0900

    백준 for문 - 10871번

---
+++ Xlower.java
@@ -0,0 +1,19 @@
+package third;
+
+import java.util.Scanner;
+
+public class Xlower {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			int A = sc.nextInt();
+			if (A < X)
+				System.out.print(A + " ");
+			sc.close();
+		}
+	}
+}
