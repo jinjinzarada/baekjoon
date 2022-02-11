0d525dbc00efdd7f9592c2c0ea58bfc4d59a6a67
jinjinzarada <jinjinzara774@gmail.com>
2022-02-11 23:40:00 +0900

    백준 while문 - 1110번

---
+++ While_4.java
@@ -0,0 +1,26 @@
+package fourth;
+
+import java.util.Scanner;
+
+public class While_4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		sc.close();
+
+		int cnt = 0;
+		int copy = A;
+
+		while (true) {
+			A = ((A % 10) * 10) + (((A / 10) + (A % 10)) % 10);
+			cnt++;
+
+			if (copy == A) {
+				break;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
