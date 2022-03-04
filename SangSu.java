859cff232ff56e9cc5ce8435e7dcfc9049ddaa38
jinjinzarada <jinjinzara774@gmail.com>
2022-03-04 23:44:33 +0900

    백준 문자열 - 2908번

---
+++ SangSu.java
@@ -0,0 +1,22 @@
+package fifth;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SangSu {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+		System.out.print(A > B ? A : B);
+		br.close();
+
+	}
+
+}
