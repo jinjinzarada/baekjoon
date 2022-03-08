e878c6854112e8724a34f931ca6b5d02951f3951
jinjinzarada <jinjinzara774@gmail.com>
2022-03-08 21:21:23 +0900

    백준 함수 - 15596번

---
+++ Function.java
@@ -0,0 +1,18 @@
+package fifth;
+
+public class Function {
+
+	public static void main(String[] args) {
+		class Test {
+			long sum(int[] s) {
+				int sum = 0;
+				for (int i = 0; i < s.length; i++) {
+					sum += s[i];
+				}
+				return sum;
+			}
+
+		}
+	}
+
+}
